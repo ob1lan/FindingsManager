df36df2784bd1fd9669a04621a4a7230da56eab8
Antoine Delrue <antoine@a-delrue.be>
2023-11-09 21:43:04 +0100

    Updates routes

--- routes/findings.routes.js
+++ routes/findings.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { ensureAuthenticated } = require("../config/guards.config");
 const { uploadCSV } = require("../config/upload.config");
+const { uploadAttachment } = require("../config/upload.config");
 const fndCtrl = require("../controllers/findings.controller");
 
 router.get("/", ensureAuthenticated, fndCtrl.findings);
-router.post("/new-finding", ensureAuthenticated, fndCtrl.findingCreate);
+router.post("/new-finding", ensureAuthenticated, uploadAttachment.single("file"), fndCtrl.findingCreate);
 router.get("/:id/details", ensureAuthenticated, fndCtrl.findingDetails);
 router.post("/:id/edit", ensureAuthenticated, fndCtrl.findingEdit);
 router.post("/:id/delete", ensureAuthenticated, fndCtrl.findingDelete);
