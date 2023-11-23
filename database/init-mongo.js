900c96b5c32d1f911cd5d6adcd54ee9bd55f20b7
Antoine Delrue <antoine.delrue@stepstone.com>
2023-11-23 14:33:55 +0100

    Fixed Issues

--- database/init-mongo.js
+++ database/init-mongo.js
@@ -3,7 +3,7 @@ db = db.getSiblingDB("findings");
 db.users.insert({
   username: "admin",
   local: {
-    email: "admin@domain.tld",
+    email: "admin@example.com",
     password: "$2b$12$J48JuCJdhFoKW98O/8KsauXGMYhPsB7xmLBwAtTWLhvjiIAynr0G.",
   },
   avatar: "/images/default-profile.svg",
