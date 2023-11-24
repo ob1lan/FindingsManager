5182f4bda178792fc69f325d5eab2c15e9f1414a
Antoine Delrue <antoine@a-delrue.be>
2023-11-24 11:05:46 +0100

    Minor update

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,9 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    dbUrl: '',
-    cert: '',
-    key: ''
-  }
+  dbUrl: process.env.DATABASE_URL || "mongodb://localhost:27017/findings",
+  cert: path.join(__dirname, "../ssl/local.crt"),
+  key: path.join(__dirname, "../ssl/local.key"),
+  portHttp: 3000,
+  portHttps: 3001,
+};
