3e814d5d751d4f7d4a1f858f268def22c80d4979
Antoine Delrue <antoine@a-delrue.be>
2023-11-27 23:51:56 +0100

    Fixed the search parameter

--- public/javascripts/findingsDatatable.js
+++ public/javascripts/findingsDatatable.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
+  function getUrlVars() {
+    var vars = [],
+      hash;
+    var hashes = window.location.href
+      .slice(window.location.href.indexOf("?") + 1)
+      .split("&");
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split("=");
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+  var searchTerm = getUrlVars()["search"];
+
   var table = $("table").DataTable({
-    select: false,
+    select: true,
     stateSave: true,
     responsive: true,
     dom: 'Bf<"clear">rtipl',
@@ -27,6 +42,19 @@ $(document).ready(function () {
       { targets: [4, 10, 11, 13], visible: false },
     ],
     colReorder: true,
+    stateLoadParams: function (settings, data) {
+      // Override the search term in the saved state if there's a URL parameter
+      if (searchTerm) {
+        data.search.search = searchTerm;
+      } else {
+        // If no search term in URL, clear the saved search term
+        data.search.search = "";
+      }
+    },
+    // Initialize the search with the URL parameter if present
+    search: {
+      search: searchTerm,
+    },
   });
   $("table tbody").on("dblclick", "tr", function () {
     var findingId = $(this).data("finding-id");
