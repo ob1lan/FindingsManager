11da04c16c1a2a557241f076929c887b3a750a2d
Antoine Delrue <antoine@a-delrue.be>
2023-12-01 21:53:33 +0100

    Minor changes

--- database/models/settings.model.js
+++ database/models/settings.model.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const smtpSettingsSchema = schema({
-  smtpHost: { type: String, required: true },
-  smtpPort: { type: Number, required: true },
+  smtpHost: { type: String, required: true, default: "127.0.0.1" },
+  smtpPort: { type: Number, required: true, default: "1025" },
   smtpUsername: { type: String, required: false },
   smtpPassword: { type: String, required: false },
-  smtpSecure: { type: Boolean, required: true },
+  smtpSecure: { type: Boolean, required: true, default: false },
 });
 
 const SLASettingsSchema = schema({
-  info: { type: Number, required: true, default: 0 },
+  info: { type: Number, required: true, default: 365 },
   low: { type: Number, required: true, default: 270 },
   medium: { type: Number, required: true, default: 90 },
   high: { type: Number, required: true, default: 14 },
